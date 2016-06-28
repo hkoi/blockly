8655c8a45b22e576e61eff4478e1f12a4d972754
Tony Wong <microtony@gmail.com>
2016-06-28 13:05:53 +0800

    add jsdom

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,15 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename');
 
+var wrap1 = 'var DOMParser = require("xmldom").DOMParser;\n';
+wrap1 += 'var XMLSerializer = require("xmldom").XMLSerializer;\n';
+wrap1 += 'var document = require("jsdom").jsdom();\n';
+wrap1 += 'module.exports = (function() { var that = {}; that.navigator = "";\n';
+
 gulp.task('default', function() {
   return gulp.src(['blockly_compressed.js', './blocks_compressed.js', './javascript_compressed.js', './msg/js/en.js'])
       .pipe(concat('blockly_wrapped.js'))
       .pipe(replace(/goog\.global\s*=\s*this;/, 'goog.global = that;'))
-      .pipe(insert.wrap('var DOMParser = require("xmldom").DOMParser;\nvar XMLSerializer = require("xmldom").XMLSerializer;\nmodule.exports = (function() { var that = {}; that.navigator = ""; ', ' return Blockly; })()'))
+      .pipe(insert.wrap(wrap1, ' return Blockly; })()'))
       .pipe(gulp.dest("."));
 });
